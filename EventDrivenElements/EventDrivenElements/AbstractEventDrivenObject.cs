8be10bbcbf21d711f89e7a89dd2669b4ac55b4ae
KailangHuo <hkailang@student.unimelb.edu.au>
2023-09-18 23:31:22 +0800

    更新 :  更新说明文字信息

--- EventDrivenElements/EventDrivenElements/AbstractEventDrivenObject.cs
+++ EventDrivenElements/EventDrivenElements/AbstractEventDrivenObject.cs
@@ -1,19 +1,64 @@
 namespace EventDrivenElements; 
 
+/**
+ * Author: Kyan Huo
+ * E-mail address : kyanhuo.kyan@gmail.com
+ *
+ *  * the comments are written in duo language Eng - Cn
+ *  * 本文的注释是双语注释 Eng - 中文
+ *
+ *  This abstract class should be implemented if an object demands to 'subscribe' other objects or
+ *  publish their events to other objects.
+ *
+ *  You may find more example about this architecture from my Github page:
+ *  你可以在我的 Github 页面找到更多这个结构的使用示例
+ *  https://github.com/KailangHuo?tab=repositories
+ *
+ * 
+ */
+
+
 public abstract class AbstractEventDrivenObject : IEventDrivenObserver {
     
+    /// <summary>
+    /// This list contains implementing instances of IEventDrivenObserver
+    /// who are subscribing to this object. This list should only contains objects that belongs
+    /// to Model layer
+    ///
+    /// 观察者容器, 只存来自Model层的对象
+    /// </summary>
     private List<IEventDrivenObserver> _observers;
-
+    
+    
+    /// <summary>
+    /// This list contains implementing instances of AbstractEventDrivenViewModel
+    /// who are subscribing to this object. This list should only contains objects that belongs
+    /// to View Model layer.
+    ///
+    /// ViewModel观察者容器, 只存来自View Model层的对象
+    /// </summary>
     private List<AbstractEventDrivenViewModel> _viewModelObservers;
 
+    
+    /// <summary>
+    /// This map maintains published events.
+    ///
+    /// 已发布事件容器, 所有发布过的事件都会在这个容器里面更新
+    /// </summary>
     private Dictionary<string, object> _publishingValueMap;
 
+    
     public AbstractEventDrivenObject() {
         _observers = new List<IEventDrivenObserver>();
         _viewModelObservers = new List<AbstractEventDrivenViewModel>();
         _publishingValueMap = new Dictionary<string, object>();
     }
 
+    /// <summary>
+    /// Event publishing will always notify Models first and then the View Models
+    /// </summary>
+    /// <param name="PropertyName"></param>
+    /// <param name="Parameter"></param>
     public void PublishEvent(string PropertyName, object Parameter) {
         if (_publishingValueMap.ContainsKey(PropertyName)) _publishingValueMap[PropertyName] = Parameter;
         else _publishingValueMap.Add(PropertyName, Parameter);
@@ -26,6 +71,12 @@ public abstract class AbstractEventDrivenObject : IEventDrivenObserver {
         }
     }
 
+    /// <summary>
+    /// Subscribers registration.
+    /// After a registration, this object will notify to the subscriber immediately
+    /// about its published events that saved in the _publishingValueMap.
+    /// </summary>
+    /// <param name="o"></param>
     public void RegisterObserver(IEventDrivenObserver o) {
         if (o is AbstractEventDrivenViewModel) RegisterViewModel((AbstractEventDrivenViewModel)o);
         else if(!this._observers.Contains(o)) this._observers.Add(o);
